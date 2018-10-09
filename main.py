a67b9db5e9405e31640abe938c5eb0fd0aa5fc03
michaelnicolosi <mike@mikenicolosi.com>
2018-10-09 19:29:04 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,41 @@
+
+
+# Copyright 2018 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# [START gae_python37_app]
+from flask import Flask
+import requests
+
+# If `entrypoint` is not defined in app.yaml, App Engine will look for an app
+# called `app` in `main.py`.
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    """Return a friendly HTTP greeting."""
+    return 'Hello World!'
+
+ga_url = 'https://www.google-analytics.com/collect?v=1&t=event&tid=UA-121324737-1&cid=2388949.389898989&ec=appengine_lead_test&ea=test1&el=123435'
+r = requests.post(url = ga_url) 
+print(r.url)
+
+
+if __name__ == '__main__':
+    # This is used when running locally only. When deploying to Google App
+    # Engine, a webserver process such as Gunicorn will serve the app. This
+    # can be configured by adding an `entrypoint` to app.yaml.
+    app.run(host='127.0.0.1', port=8080, debug=True)
+# [END gae_python37_app]
